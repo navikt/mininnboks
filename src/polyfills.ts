ffeaef8d9fcd3376472eb6248c75c9117bd86eb1
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-04-30 09:05:40 +0200

    [OPP-808] Fikset tslint-feil

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -13,17 +13,17 @@ if (!global.Intl) {
     }
     const console: Console = {} as Console;
     const empty = {};
-    const dummy = function dummy() {};
+    const dummy = () => {}; // tslint:disable-line no-empty
     const properties = 'memory'.split(',');
     const methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
         'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
         'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn').split(',');
     properties.forEach((prop) => {
-        console[prop] = empty; // eslint-disable-line no-param-reassign
+        console[prop] = empty;
     });
     methods.forEach((method) => {
-        console[method] = dummy; // eslint-disable-line no-param-reassign
+        console[method] = dummy;
     });
 
-    (window as any).console = console;
+    (window as any).console = console; // tslint:disable-line no-any
 })(window.console));
