f7d5202efa403d52057fd55e20cfa21896e0ef56
Vilde Stefferud <qwartz.ms@hotmail.com>
2020-08-26 14:45:21 +0200

    [OPP1222] konverterer til typescript, mangler å konvertere props

--- src/dokument-visning/DokumentVisning.tsx
+++ src/dokument-visning/DokumentVisning.tsx
@@ -1,62 +1,57 @@
 import PT from 'prop-types';
-import React from 'react';
+import* as React from 'react';
 import Lenke from 'nav-frontend-lenker';
 import { FormattedMessage } from 'react-intl';
 import Personalia from './dokumentvisning/personalia/personalia';
 import Dokumenter from './dokumentvisning/dokument/dokumenter';
 import IntlLenke from "../utils/IntlLenke";
 import './dokument-visning.less';
+import {useEffect} from "react";
+import Dokument from './dokumentvisning/dokument/dokument';
 
-class DokumentVisning extends React.Component {
-    componentDidMount() {
-        document.body.scrollTop = 1;
-        document.documentElement.scrollTop = 1;
-    }
 
-    render() {
-        const {
-            dokumentmetadata,
-            journalpostmetadata,
-            lastNedPdfOnClick,
-            printPdfOnClick
-        } = this.props;
-        const { temakode } = journalpostmetadata.resultat;
-
-        return (
-            <div className="dokinnsyn">
-                <section className="dokumentvisning-header blokk-m">
-                    <Personalia journalpostmetadata={journalpostmetadata.resultat} hode={false} className="blokk-m" />
-                    <ul className="ustilet">
-                        <li>
-                            <Lenke href={`/saksoversikt/app/tema/${temakode}`}>
-                                <FormattedMessage id="dokumentvisning.gatil.saksoversikt" />
-                            </Lenke>
-                        </li>
-                        <li>
-                            <IntlLenke href="skriv.ny.link" className="lenke">
-                                <FormattedMessage id="dokumentvisning.kontakt.nav" />
-                            </IntlLenke>
-                        </li>
-                    </ul>
-                </section>
-                <section className="dokumenter">
-                    <Dokumenter
-                        journalpostId={journalpostmetadata.resultat.journalpostId}
-                        dokumentmetadata={dokumentmetadata}
-                        lastNedPdfOnClick={lastNedPdfOnClick}
-                        printPdfOnClick={printPdfOnClick}
-                    />
-                </section>
-            </div>
-        );
-    }
-}
-
-DokumentVisning.propTypes = {
-    dokumentmetadata: PT.array.isRequired,
+interface Props {
+    dokumentmetadata: Array<Dokument>,
     lastNedPdfOnClick: PT.func.isRequired,
     printPdfOnClick: PT.func.isRequired,
-    journalpostmetadata: PT.object.isRequired
-};
+    journalpostmetadata: Personalia
+}
+
+function DokumentVisning(props : Props) {
+    useEffect(() => {
+        document.body.scrollTop = 1;
+        document.documentElement.scrollTop = 1;
+    }, [])
+
+    const { temakode } = props.journalpostmetadata.resultat;
+
+    return (
+        <div className="dokinnsyn">
+            <section className="dokumentvisning-header blokk-m">
+                <Personalia journalpostmetadata={props.journalpostmetadata.resultat} hode={false} className="blokk-m" />
+                <ul className="ustilet">
+                    <li>
+                        <Lenke href={`/saksoversikt/app/tema/${temakode}`}>
+                            <FormattedMessage id="dokumentvisning.gatil.saksoversikt" />
+                        </Lenke>
+                    </li>
+                    <li>
+                        <IntlLenke href="skriv.ny.link" className="lenke">
+                            <FormattedMessage id="dokumentvisning.kontakt.nav" />
+                        </IntlLenke>
+                    </li>
+                </ul>
+            </section>
+            <section className="dokumenter">
+                <Dokumenter
+                    journalpostId={props.journalpostmetadata.resultat.journalpostId}
+                    dokumentmetadata={props.dokumentmetadata}
+                    lastNedPdfOnClick={props.lastNedPdfOnClick}
+                    printPdfOnClick={props.printPdfOnClick}
+                />
+            </section>
+        </div>
+    );
+}
 
 export default DokumentVisning;
