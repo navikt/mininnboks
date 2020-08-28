3825efc2ae6607930c417ebc1cc14ff1bbe23c13
Vilde Stefferud <qwartz.ms@hotmail.com>
2020-08-28 12:59:57 +0200

    [OPP1222] konvertert fra javascript til typescript

--- src/dokument-visning/dokumentvisning/personalia/Personalia.tsx
+++ src/dokument-visning/dokumentvisning/personalia/Personalia.tsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import PT from 'prop-types';
+import * as React from 'react';
 import { FormattedDate } from 'react-intl';
-import AvsenderMottaker from './avsender-mottaker';
+import AvsenderMottaker from './AvsenderMottaker';
 import {Undertittel } from 'nav-frontend-typografi';
 import moment from 'moment';
+import {Melding} from "../../../Traad";
+
+
+interface Props {
+    hode: boolean,
+    journalpostmetadata: Journalpostmetadata
+}
+
+interface Journalpostmetadata {
+    retning: string,
+    navn: string,
+    avsender: string,
+    mottaker: string,
+    dato: string
+    journalPostId: string
+}
 
 const UKJENT_JOURNALPOSTID = 'x';
-const toDate = (dato) => {
+const toDate = (dato : string) => {
     return moment(dato).toDate();
 };
 
-const erGyldigJournalpost = (metadata) => {
-    return metadata && metadata.journalpostId && metadata.journalpostId !== UKJENT_JOURNALPOSTID;
+const erGyldigJournalpost = (journalPostId : string) => {
+    return journalPostId && journalPostId !== UKJENT_JOURNALPOSTID;
 };
 
-function Personalia({ journalpostmetadata, className, hode }) {
-    const { dato, retning, mottaker, avsender, navn } = journalpostmetadata;
-    if (!erGyldigJournalpost(journalpostmetadata)) {
+function Personalia(props : Props) {
+    if (!erGyldigJournalpost(props.journalpostmetadata.journalPostId)) {
         return null;
     }
     return (
         <section className={className}>
             <Undertittel tag="h1">
-                <AvsenderMottaker retning={retning} mottaker={mottaker} avsender={avsender} navn={navn}/>
+                <AvsenderMottaker
+                    retning={props.journalpostmetadata.retning}
+                    mottaker={props.journalpostmetadata.mottaker}
+                    avsender={props.journalpostmetadata.avsender}
+                    navn={props.journalpostmetadata.navn}
+                />
             </Undertittel>
             <p className="typo-undertittel text-center">
                 <FormattedDate
-                    value={toDate(dato)}
+                    value={toDate(props.journalpostmetadata.dato)}
                     day="numeric"
                     month="long"
                     year="numeric"
@@ -38,17 +57,6 @@ function Personalia({ journalpostmetadata, className, hode }) {
 
 Personalia.defaultProps = {
     hode: true
-};
-
-Personalia.propTypes = {
-    hode: PT.bool,
-    journalpostmetadata: PT.shape({
-        retning: PT.string.isRequired,
-        navn: PT.string,
-        avsender: PT.string.isRequired,
-        mottaker: PT.string.isRequired,
-        dato: PT.string.isRequired
-    }).isRequired
 };
 
 export default Personalia;
