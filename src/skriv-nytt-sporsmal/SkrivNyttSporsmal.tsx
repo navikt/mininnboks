fd7b5652caaa0d101820e3bff4b83e8ddae4daec
Nicklas Utgaard <nutgaard@gmail.com>
2021-01-28 10:33:38 +0100

    [KAIZEN-0] forbedringer til ts og skriv-nytt-sporsmal oppsett

--- src/skriv-nytt-sporsmal/SkrivNyttSporsmal.tsx
+++ src/skriv-nytt-sporsmal/SkrivNyttSporsmal.tsx
@@ -1,173 +1,111 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { Dispatch } from 'redux';
-import { skjulVilkarModal, TypeKeys, visVilkarModal } from '../ducks/ui';
-import { sendSporsmal } from '../ducks/traader';
-import { STATUS } from '../ducks/ducks-utils';
+import { useLocation, useParams } from 'react-router';
 import { Textarea } from 'nav-frontend-skjema';
-import GodtaVilkar from './GodtaVilkar';
-import Kvittering from './Kvittering';
-import TemagruppeEkstraInfo, { Temagruppe } from './TemagruppeEkstraInfo';
-import Feilmelding from '../feilmelding/Feilmelding';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { Innholdstittel, Normaltekst, Sidetittel, Undertittel } from 'nav-frontend-typografi';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import Alertstripe, { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
-
-import './skriv-nytt-sporsmal.less';
+import Alertstripe from 'nav-frontend-alertstriper';
+import { Values } from '@nutgaard/use-formstate';
+import { hasError, isPending } from '@nutgaard/use-fetch';
+import { sendSporsmal } from '../ducks/traader';
+import GodtaVilkar from './GodtaVilkar';
+import Kvittering from './Kvittering';
+import TemagruppeEkstraInfo from './TemagruppeEkstraInfo';
+import Feilmelding from '../feilmelding/Feilmelding';
 import { feilmelding } from '../utils/validationutil';
-import { visibleIfHOC } from '../utils/hocs/visible-if';
-import { harTilgangTilKommunaleTemagrupper, TilgangState } from '../ducks/tilgang';
-import { sjekkOgOppdaterRatelimiter, sjekkRatelimiter } from '../utils/api';
-import { useLocation, useParams } from 'react-router';
-import { AppState } from '../reducer';
 import Spinner from '../utils/Spinner';
-import useFormstateFactory, { Values } from '@nutgaard/use-formstate';
-import { Temagrupper } from '../utils/constants';
-import { useAppState, useOnMount, useThunkDispatch } from '../utils/custom-hooks';
-import { hentLedetekster } from '../ducks/ledetekster';
-import { laster, harFeil } from '../avhengigheter';
+import { Temagruppe, TemagruppeNavn } from '../utils/constants';
+import { useThunkDispatch } from '../utils/custom-hooks';
+import {
+    AlertstripeAdvarselVisibleIf,
+    AndreFeilmeldinger,
+    SkrivNyttSporsmalForm,
+    useFormstate,
+    useRatelimiter
+} from './common';
+import './skriv-nytt-sporsmal.less';
+import { useLedetekster } from '../utils/api';
 
-const AlertstripeAdvarselVisibleIf = visibleIfHOC(AlertStripeAdvarsel);
+const spesialtHandterteTemagrupper = [Temagruppe.FDAG];
 
-interface Props {
-    actions: {
-        visVilkarModal: () => { type: TypeKeys; data: boolean };
-        skjulVilkarModal: () => { type: TypeKeys; data: boolean };
-    };
-    skalViseVilkarModal: boolean;
-    sendingStatus: string;
-    tilgang: TilgangState;
-}
-
-type SkrivNyttSporsmalForm = {
-    fritekst: string;
-    godkjennVilkaar: string;
-};
-
-export const useFormstate = useFormstateFactory<SkrivNyttSporsmalForm>({
-    godkjennVilkaar(value) {
-        return value === 'true' ? undefined : 'Du må godta vilkårene for å sende beskjeden';
-    },
-    fritekst(value: string) {
-        if (value.length === 0) {
-            return 'Tekstfeltet er tomt';
-        } else if (value.length > 1000) {
-            return 'Teksten er for lang';
-        }
-        return undefined;
-    }
-});
-
-enum FeilmeldingKommunalSjekk {
-    FEILET = 'Noe gikk galt, vennligst prøv igjen på ett senere tidspunkt.',
-    KODE6 = 'Du har dessverre ikke mulighet til å benytte denne løsningen. Vi ber om at du kontakter oss på telefon.',
-    INGEN_ENHET = 'Du har dessverre ikke mulighet til å benytte denne løsningen. Vi ber om at du kontakter oss på telefon.'
-}
-
-function SkrivNyttSporsmal(props: Props) {
-    const [rateLimiter, setRateLimiter] = useState(true);
-    const params = useParams<{ temagruppe: Temagruppe }>();
+function SkrivNyttSporsmal() {
     const dispatch = useThunkDispatch();
-
-    const state = useFormstate({
-        fritekst: '',
-        godkjennVilkaar: 'false'
-    });
-
-    const ledetekster = useAppState((state) => state.ledetekster);
-    useOnMount(() => {
-        const temagruppe = params.temagruppe.toLowerCase();
-        dispatch(hentLedetekster());
-        if (temagruppe === 'oksos') {
-            dispatch(harTilgangTilKommunaleTemagrupper());
-        }
-        sjekkRatelimiter().then((res) => setRateLimiter(res));
-    });
-
+    const rateLimiter = useRatelimiter();
+    const ledetekster = useLedetekster();
+    const params = useParams<{ temagruppe: Temagruppe }>();
+    const temagruppe = params.temagruppe.toUpperCase() as Temagruppe;
     const location = useLocation();
-    const temagruppe = params.temagruppe;
     const isDirekte = location.pathname.includes('/direkte');
+    const formstate = useFormstate({ fritekst: '', godkjennVilkaar: 'false' });
 
-    if (laster(ledetekster)) {
-        return <Spinner />;
-    } else if (harFeil(ledetekster)) {
+    if (spesialtHandterteTemagrupper.includes(temagruppe)) {
         return <Alertstripe type="advarsel">Noe gikk galt, vennligst prøv igjen på ett senere tidspunkt.</Alertstripe>;
     }
-    const godkjenteTemagrupper = ledetekster.godkjenteTemagrupper;
 
-    if (temagruppe.toLowerCase() === 'oksos') {
-        if (props.tilgang.status === STATUS.PENDING) {
-            return <Spinner />;
-        } else if (props.tilgang.status === STATUS.ERROR) {
-            return (
-                <Alertstripe type="advarsel">Noe gikk galt, vennligst prøv igjen på ett senere tidspunkt.</Alertstripe>
-            );
-        } else if (props.tilgang.status === STATUS.OK && props.tilgang.data.resultat !== 'OK') {
-            return <Alertstripe type="info">{FeilmeldingKommunalSjekk[props.tilgang.data.resultat]}</Alertstripe>;
-        }
+    if (isPending(ledetekster)) {
+        return <Spinner />;
+    } else if (hasError(ledetekster)) {
+        return <Alertstripe type="advarsel">Noe gikk galt, vennligst prøv igjen på ett senere tidspunkt.</Alertstripe>;
+    }
+    const godkjenteTemagrupper = ledetekster.data['temagruppe.liste'].split(' ');
+
+    if (!godkjenteTemagrupper.includes(temagruppe)) {
+        return <Feilmelding>{AndreFeilmeldinger.IKKE_GODKJENT_TEMAGRUPPE}</Feilmelding>;
+    } else if (formstate.submittingSuccess) {
+        return <Kvittering />;
     }
 
     function submitHandler<S>(values: Values<SkrivNyttSporsmalForm>): Promise<any> {
-        return sjekkOgOppdaterRatelimiter().then((isOK) => {
+        return rateLimiter.update().then((isOK) => {
             if (isOK) {
                 return dispatch(sendSporsmal(temagruppe, values.fritekst, isDirekte));
             } else {
-                setRateLimiter(isOK);
-                return new Promise((resolve, reject) => reject('rate-limiter feilmelding'));
+                return Promise.reject('rate-limiter feilmelding');
             }
         });
     }
 
-    if (!godkjenteTemagrupper.includes(temagruppe)) {
-        return <Feilmelding>Ikke gjenkjent temagruppe</Feilmelding>;
-    } else if (props.sendingStatus === STATUS.OK) {
-        return <Kvittering />;
-    }
-
-    const valgtTemagruppe = Temagrupper[temagruppe];
     return (
         <article className="blokk-center send-sporsmal-side skriv-nytt-sporsmal">
             <Sidetittel className="text-center blokk-m">Send beskjed til NAV</Sidetittel>
-            <form className="panel text-center" onSubmit={state.onSubmit(submitHandler)}>
+            <form
+                className="panel text-center"
+                onSubmit={formstate.onSubmit(submitHandler, { preventConcurrent: true })}
+            >
                 <i className="meldingikon" />
                 <Innholdstittel className="blokk-xl">Skriv melding</Innholdstittel>
-                <Undertittel className="blokk-s">{valgtTemagruppe}</Undertittel>
+                <Undertittel className="blokk-s">{TemagruppeNavn[temagruppe]}</Undertittel>
                 <div className="blokk-xs">
                     <AlertstripeAdvarselVisibleIf visibleIf={!rateLimiter}>
                         Du har oversteget antall meldinger som kan sendes til NAV på kort tid. Prøv igjen på ett senere
-                        tidspunkt.{' '}
+                        tidspunkt.
                     </AlertstripeAdvarselVisibleIf>
-                    <AlertstripeAdvarselVisibleIf visibleIf={props.sendingStatus === STATUS.ERROR}>
+                    <AlertstripeAdvarselVisibleIf visibleIf={formstate.submittingFailed}>
                         Det har skjedd en feil med innsendingen av spørsmålet ditt. Vennligst prøv igjen senere.
                     </AlertstripeAdvarselVisibleIf>
                 </div>
                 <Normaltekst className="typo-normal blokk-xs">
                     Send bare beskjeder som kan ha betydning for saken din. Husk å få med alle relevante opplysninger.
-                    Du kan skrive maksimalt 1000 tegn, det er cirka en halv A4-side.{' '}
+                    Du kan skrive maksimalt 1000 tegn, det er cirka en halv A4-side.
                 </Normaltekst>
-                <TemagruppeEkstraInfo temagruppe={temagruppe} key={temagruppe} />
+                <TemagruppeEkstraInfo temagruppe={temagruppe} />
                 <div className="text-center margin">
                     <Textarea
                         textareaClass="fritekst"
                         label={''}
                         maxLength={1000}
-                        {...state.fields.fritekst.input}
-                        feil={feilmelding(state.fields.fritekst)}
+                        {...formstate.fields.fritekst.input}
+                        feil={feilmelding(formstate.fields.fritekst)}
                     />
                     <GodtaVilkar
                         label="Jeg godtar vilkårene for bruk av tjenesten."
-                        visModal={props.skalViseVilkarModal}
-                        actions={props.actions}
-                        fieldstate={state.fields.godkjennVilkaar}
-                        feil={feilmelding(state.fields.godkjennVilkaar)}
+                        fieldstate={formstate.fields.godkjennVilkaar}
+                        feil={feilmelding(formstate.fields.godkjennVilkaar)}
                     />
                     <Hovedknapp
                         htmlType="submit"
                         type="hoved"
-                        spinner={props.sendingStatus === STATUS.PENDING}
-                        aria-disabled={props.sendingStatus === STATUS.PENDING}
+                        spinner={formstate.submitting}
+                        aria-disabled={formstate.submitting}
                     >
                         Send
                     </Hovedknapp>
@@ -177,16 +115,4 @@ function SkrivNyttSporsmal(props: Props) {
     );
 }
 
-const mapStateToProps = ({ traader, ui, tilgang }: AppState) => ({
-    skalViseVilkarModal: ui.visVilkarModal,
-    sendingStatus: traader.innsendingStatus,
-    tilgang: tilgang
-});
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    actions: {
-        visVilkarModal: () => dispatch(visVilkarModal()),
-        skjulVilkarModal: () => dispatch(skjulVilkarModal())
-    }
-});
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SkrivNyttSporsmal));
+export default SkrivNyttSporsmal;
