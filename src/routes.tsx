d873eb9beda111beb3619e076a6c2c6345d27156
Nicklas Utgaard <nutgaard@gmail.com>
2021-08-17 12:27:27 +0200

    [OPP-1378] avblending av url-routes som ikke skal fungere etter overgang

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Listevisning from './listevisning/ListeVisning';
 import Traadvisning from './traadvisning/TraadVisning';
 import Oppgavevisning from './oppgave-visning/OppgaveVisning';
@@ -8,22 +8,37 @@ import DokumentVisningSide from './dokument-visning/DokumentVisningSide';
 import Traader from './traader/Traader';
 import Brodsmuler from './brodsmuler/Brodsmuler';
 import SkrivNyttSporsmalFDAG from "./skriv-nytt-sporsmal/SkrivNyttSporsmalFDAG";
+import NyDialogLosning from "./ny-dialog-losning/ny-dialog-losning";
+import { useFeaturetoggles } from "./utils/api";
+import { hasError, isPending } from "@nutgaard/use-fetch";
+import { AlertStripeAdvarsel } from "nav-frontend-alertstriper";
 
 export default function () {
+    const featuretoggles = useFeaturetoggles();
+    if (hasError(featuretoggles)) {
+        return <AlertStripeAdvarsel>Det skjedde en feil ved lasting av featuretoggles</AlertStripeAdvarsel>;
+    } else if (isPending(featuretoggles)) {
+        return null;
+    }
+    const brukerSFSomBackend: boolean = featuretoggles.data["modiabrukerdialog.bruker-salesforce-dialoger"];
+
     return (
         <BrowserRouter>
             <Brodsmuler />
+            <NyDialogLosning visibleIf={brukerSFSomBackend} />
             <Switch>
-                <Route exact path="/sporsmal/skriv/FDAG" component={SkrivNyttSporsmalFDAG}/>
-                <Route exact path="/sporsmal/skriv/:temagruppe/" component={SkrivNyttSporsmal} />
-                <Route exact path="/sporsmal/skriv/:temagruppe/direkte" component={SkrivNyttSporsmal} />
+                { !brukerSFSomBackend && <Route exact path="/sporsmal/skriv/FDAG" component={SkrivNyttSporsmalFDAG}/> }
+                { !brukerSFSomBackend && <Route exact path="/sporsmal/skriv/:temagruppe/" component={SkrivNyttSporsmal} /> }
+                { !brukerSFSomBackend && <Route exact path="/sporsmal/skriv/:temagruppe/direkte" component={SkrivNyttSporsmal} /> }
+                { brukerSFSomBackend && <Redirect from="/sporsmal" to="/" /> }
                 <Route>
                     <Traader>
                         <Switch>
-                            <Route exact path="/traad/:traadId" component={Traadvisning} />
+                            { !brukerSFSomBackend && <Route exact path="/traad/:traadId" component={Traadvisning} /> }
+                            { brukerSFSomBackend && <Redirect from="/traad" to="/" /> }
                             <Route exact path="/dokument/:id" component={DokumentVisningSide} />
                             <Route exact path="/oppgave/:id" component={Oppgavevisning} />
-                            <Route component={Listevisning} />
+                            <Route render={() => <Listevisning brukerSFSomBackend={brukerSFSomBackend}/>} />
                         </Switch>
                     </Traader>
                 </Route>
