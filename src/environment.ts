e52fb906c3a2702079960dafe9bda42ed09988f4
Nicklas Utgaard <nutgaard@gmail.com>
2021-08-17 12:26:19 +0200

    [OPP-1378] hente ut url til SF løsning fra miljøvariabel

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,20 @@
 declare global {
     interface Window {
-        mininnboks: { NAMESPACE?: string };
+        mininnboks?: {
+            NAMESPACE: string;
+            SF_DIALOG_URL: string;
+        };
     }
 }
 
-export function getNAVBaseUrl(): String {
+export function getNAVBaseUrl(): string {
     const environment = window.mininnboks?.NAMESPACE;
     if (environment !== 'p') {
         return `https://www-${environment}.nav.no`;
     }
     return 'https://www.nav.no';
+}
+
+export function getSfUrl(): string {
+    return window.mininnboks?.SF_DIALOG_URL || getNAVBaseUrl();
 }
