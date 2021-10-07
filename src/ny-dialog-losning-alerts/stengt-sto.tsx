c11be863725b13fc289de5bc96c96b543188f141
Nicklas Utgaard <nutgaard@gmail.com>
2021-10-07 15:04:48 +0200

    [KAIZEN-0] oppdatere tekst

--- src/ny-dialog-losning-alerts/stengt-sto.tsx
+++ src/ny-dialog-losning-alerts/stengt-sto.tsx
@@ -1,14 +1,17 @@
-import * as React from "react";
+import * as React from 'react';
 import { Systemtittel, Normaltekst } from 'nav-frontend-typografi';
 import { AlertStripeInfo } from 'nav-frontend-alertstriper';
-import { visibleIfHOC } from "../utils/hocs/visible-if";
+import { visibleIfHOC } from '../utils/hocs/visible-if';
 import './sf-info-alertstriper.less';
 
 function StengtSTO() {
     return (
         <AlertStripeInfo size="4rem" className="blokk-xl alertstripe--utvided">
             <Systemtittel>Innsending av nye meldinger er midlertidig stengt.</Systemtittel>
-            <Normaltekst>Det er for tiden ikke mulig å sende melding til NAV, vi beklager dette.</Normaltekst>
+            <Normaltekst>
+                Det er for tiden ikke mulig å sende melding til NAV på grunn av teknisk endring som utføres på
+                innboksen. Vi beklager dette.
+            </Normaltekst>
         </AlertStripeInfo>
     );
 }
